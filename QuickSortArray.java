023a092f9e851795021eea1d0f62feee4bb7320c
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 11:00:14 +0200

    Update QuickSortArray.java

--- QuickSortArray.java
+++ QuickSortArray.java
@@ -30,20 +30,4 @@ public class QuickSort {
 		list[high] = temp;
 		return i+1;
 	}
-	
-	public static void main(String[] args) {
-		int n = 10000;
-		int[] list = new int[800];
-		Random rnd = new Random();
-		
-		for (int i = 0; i < 800; i++) {
-			list[i] = rnd.nextInt(n);
-		}
-	
-		sort(list);
-		
-		for(int i = 0; i < 800; i++){
-			System.out.println(list[i] + " ");
-		}
-	}
 }

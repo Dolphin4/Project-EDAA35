411e7da2479a5525ff0a9437df9e2b687bc16da3
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 10:51:44 +0200

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -2,29 +2,29 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class MergeSort{
-		private static LinkedList<Integer> list;
+	private static LinkedList<Integer> list;
         private static LinkedList<Integer> tempList;
         private static int length;
 		
-		public static LinkedList<Integer> sort(LinkedList<Integer> list2){
-			list = list2;
-			length = list.size();
-			tempList = new LinkedList<Integer>();
-			mergesort(0, length - 1);
+	public static LinkedList<Integer> sort(LinkedList<Integer> list2){
+		list = list2;
+		length = list.size();
+		tempList = new LinkedList<Integer>();
+		mergesort(0, length - 1);
 			
-			return list;
-		}
+		return list;
+	}
 
-		private static void mergesort(int low, int high) {
-            if (low < high) {
-                int middle = low + (high - low) / 2;
-                mergesort(low, middle);
-                mergesort(middle + 1, high);
-                merge(low, middle, high);
-            }
-		}
+	private static void mergesort(int low, int high) {
+        	if (low < high) {
+                	int middle = low + (high - low) / 2;
+                	mergesort(low, middle);
+                	mergesort(middle + 1, high);
+                	merge(low, middle, high);
+            	}
+	}
 		
-		private static void merge(int low, int middle, int high) {
+	private static void merge(int low, int middle, int high) {
             for (int i = low; i <= high; i++) {
                 tempList.add(i, list.get(i));
             }
@@ -34,11 +34,11 @@ public class MergeSort{
             
             while (i <= middle && j <= high) {
             	if (tempList.get(i).compareTo(tempList.get(j)) <= 0) {
-					list.set(k, tempList.get(i));
-                    i++;
-                }else {
+			list.set(k, tempList.get(i));
+                    	i++;
+                } else {
                 	list.set(k, tempList.get(j));
-                    j++;
+                    	j++;
                 }
                 k++;
             }
@@ -48,5 +48,5 @@ public class MergeSort{
                 k++;
                 i++;
             }
-		}
+	}
 }

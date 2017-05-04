67f52b4edc85465d58c0e5f125b5c2b0033761d4
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 12:18:16 +0200

    Update InsertionSortArray.java

--- InsertionSortArray.java
+++ InsertionSortArray.java
@@ -1,14 +1,15 @@
-public class InsertionSort {
-	public static int[] sort(int[] list){
-		for(int i = 0; i < list.length; i++){
-			int tempi = list[i];
-			int j = i - 1;
-			while(j >= 0 &&  list[j] > tempi){
-				list[j + 1] = list[j];
-				j--;
-			}
-			list[j + 1] = tempi;
-		}
-		return list;	
+public class InsertionSortArray{
+	public static int[] sort(int[] input){
+		int temp;
+	    for (int i = 1; i < input.length; i++) {
+	        for(int j = i ; j > 0 ; j--){
+	            if(input[j] < input[j-1]){
+	                temp = input[j];
+	                input[j] = input[j-1];
+	                input[j-1] = temp;
+	            }
+	        }
+	    }
+	    return input;	
 	}
 }

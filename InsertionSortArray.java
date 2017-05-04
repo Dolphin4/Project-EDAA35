3390018f5d8be76b5a42fea44254fbfd3a3f739e
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 10:46:36 +0200

    Update InsertionSortArray.java

--- InsertionSortArray.java
+++ InsertionSortArray.java
@@ -13,20 +13,4 @@ public class InsertionSort {
 		}
 		return list;	
 	}
-	
-//	public static void main(String[] args) {
-//		int n = 10000;
-//		int[] list = new int[800];
-//		Random rnd = new Random();
-//		
-//		for (int i = 0; i < 800; i++) {
-//			list[i] = rnd.nextInt(n);
-//		}
-//		
-//		sort(list);
-//		for(int i = 0; i < 800; i++){
-//			System.out.println(list[i] + " ");
-//		}
-//		
-//	}
 }

94a7daaac12f764478be24bd1f335f1a8e477e8b
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 10:50:26 +0200

    Update HeapSortArray.java

--- HeapSortArray.java
+++ HeapSortArray.java
@@ -42,19 +42,4 @@ public class HeapSort {
 		table[parent] = table[child];
 		table[child] = temp;
 	}
-
-	public static void main(String[] args) {
-		int n = 10000;
-		int[] list = new int[800];
-		Random rnd = new Random();
-		
-		for (int i = 0; i < 800; i++) {
-			list[i] = rnd.nextInt(n);
-		}
-		
-		sort(list);
-		for(int i = 0; i < 800; i++){
-			System.out.println(list[i] + " ");
-		}
-	}
 }

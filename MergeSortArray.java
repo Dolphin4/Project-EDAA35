c170d656010ddce521c8e7f6e710454278fecffd
Dolphin4 <martin.a.lundgren@hotmail.com>
2017-05-04 10:44:26 +0200

    Update MergeSortArray.java

--- MergeSortArray.java
+++ MergeSortArray.java
@@ -1,28 +1,11 @@
 import java.util.LinkedList;
 import java.util.Random;
 
-
 public class MergeSort{
 		private static int[] list;
         private static int[] tempList;
         private static int length;
-        
-//    	public static void main(String[] args) {
-//    		int n = 10000;
-//    		int[] list = new int[800];
-//    		Random rnd = new Random();
-//    		
-//    		for (int i = 0; i < 800; i++) {
-//    			list[i] = rnd.nextInt(n);
-//    		}
-//    		
-//    		sort(list);
-//    		for(int i = 0; i < 800; i++){
-//    			System.out.println(list[i] + " ");
-//    		}
-//    		
-//    	}
-//        
+       
 		
 		public static int[] sort(int[] list2){
 			list = list2;
@@ -35,21 +18,15 @@ public class MergeSort{
 
 		
 		private static void mergesort(int low, int high) {
-            // check if low is smaller than high, if not then the array is sorted
             if (low < high) {
-            	// Get the index of the element which is in the middle
                 int middle = low + (high - low) / 2;
-                // Sort the left side of the array
                 mergesort(low, middle);
-                // Sort the right side of the array
                 mergesort(middle + 1, high);
-                // Combine them both
                 merge(low, middle, high);
             }
 		}
 		
 		private static void merge(int low, int middle, int high) {
-            // Copy both parts into the helper array
             for (int i = low; i <= high; i++) {
                 tempList[i] = list[i];
             }
@@ -57,8 +34,6 @@ public class MergeSort{
             int j = middle + 1;
             int k = low;
             
-            // Copy the smallest values from either the left or the right side back
-            // to the original array
             while (i <= middle && j <= high) {
             	if (tempList[i] <= tempList[j]) {
 					list[k] = tempList[i];
@@ -69,7 +44,6 @@ public class MergeSort{
                 }
                 k++;
             }
-            // Copy the rest of the left side of the array into the target array
             while (i <= middle) {
             	list[k] = tempList[i];
                 k++;
